6a2360e23adb259224f635e607f0acace3b7d078
Noelia Martínez <noeowo384@gmail.com>
2021-03-08 16:24:45 -0400

    respuestas

---
+++ src/app/pages-admin/ad-pregunta/components/respuestas/editar-respuesta/editar-respuesta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-respuesta',
+  templateUrl: './editar-respuesta.component.html',
+  styleUrls: ['./editar-respuesta.component.scss']
+})
+export class EditarRespuestaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
