1c3833075fcfa492dc8b8263d8d0bec12eeb575e
TEFA4577 <tefihvmoonwalker77746@gmail.com>
2021-07-12 14:03:15 -0400

    cambio de rutas - categorias

--- src/app/pages/curso/components/detalles-categorias/detalles-categorias.component.ts
+++ src/app/pages/curso/components/detalles-categorias/detalles-categorias.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EtiquetaService } from '../../../../services/etiqueta.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalles-categorias',
@@ -14,17 +15,17 @@ export class DetallesCategoriasComponent implements OnInit {
   estado = true;
   texto = true;
 
-  constructor(public cursoCategoria: EtiquetaService, public route: ActivatedRoute) { }
+  constructor(public cursoCategoria: EtiquetaService, public route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
 
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
     this.id = this.route.snapshot.params.id;
     this.mostrarCursoCategoria();
   }
 
   mostrarCursoCategoria() {
     this.cursoCategoria.mostrarEtiquetaCurso(this.id).subscribe(data => {
-
       this.categorias = data;
       console.log(this.categorias);
       if (this.categorias.length !== 0) {
